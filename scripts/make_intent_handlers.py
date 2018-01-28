21d27228182be180aa7931cf9dd147078e6871e7
Kellie Medlin <kelliem@fb.com>
2018-01-27 18:27:03 -0800

    Add logic to regenerate model file from events json

--- scripts/make_intent_handlers.py
+++ scripts/make_intent_handlers.py
@@ -3,29 +3,72 @@
 import json
 import os
 
-GOD_ROOT = os.path.join('..', os.path.dirname(__file__))
+GOD_ROOT = os.path.join(os.path.dirname(__file__), '..')
+
+def generate_model(events=None):
+    if not events:
+        with open(os.path.join(GOD_ROOT, 'jsonData', 'ExampleEvents1.json')) as fs:
+            events = json.load(fs)
+
+    intents = [
+        {
+            'name': 'AMAZON.{}Intent'.format(amazon_name),
+            'samples': [],
+        }
+        for amazon_name in [
+            'Cancel',
+            'Help',
+            'No',
+            'Yes',
+            'Stop',
+        ]
+    ]
+
+    for event in events:
+        for intent in [ event['intent +'], event['Intent -'] ]:
+            intents.append({
+                'name': '{}_intent'.format(intent),
+                'samples': [intent],
+            })
+
+    return {
+        "interactionModel": {
+            "languageModel": {
+                "invocationName": "god",
+                "intents": intents,
+            },
+        }
+    }
+
+def generate_functions(model=None):
+    output_lines = []
+
+    if model is None:
+        with open(os.path.join(GOD_ROOT, 'models', 'en-US.json')) as fs:
+            model = json.load(fs)
+
+    intents = model['interactionModel']['languageModel']['intents']
+
+    for intent in intents:
+        name = intent['name']
+        samples = intent['samples']
+        samples = ','.join([
+            '"{}"'.format(sample)
+            for sample in samples
+        ])
+
+        if 'AMAZON' not in name:
+            function_output = '\n'.join([
+                "'{}': function () {{".format(name),
+                "   this.context.samples = new Array({});".format(samples),
+                "   this.emitWithState('VerifyTheCurrentIntent');",
+                "}",
+            ])
+            output_lines.append(function_output)
+    return ',\n'.join(output_lines)
+
 
 if __name__ == '__main__':
-    output_lines = []
-    with open(os.path.join(GOD_ROOT, 'models', 'en-US.json')) as fs:
-        models = json.load(fs)
-        intents = models['interactionModel']['languageModel']['intents']
-
-        for intent in intents:
-            name = intent['name']
-            samples = intent['samples']
-            samples = ','.join([
-                '"{}"'.format(sample)
-                for sample in samples
-            ])
-
-            if 'AMAZON' not in name:
-                function_output = '\n'.join([
-                    "'{}': function () {{".format(name),
-                    "   this.context.samples = new Array({});".format(samples),
-                    "   this.emitWithState('VerifyTheCurrentIntent');",
-                    "}",
-                ])
-                output_lines.append(function_output)
-
-    print(',\n'.join(output_lines))
+    model = generate_model()
+    print(json.dumps(model))
+    print(generate_functions(model))
